342e7f972255fd01212b012102158c0b518e2577
rares1928 <46875836+rares1928@users.noreply.github.com>
2020-12-23 00:27:01 +0200

    Update homePage si template grila

--- screens/GrilaRares.tsx
+++ screens/GrilaRares.tsx
@@ -7,28 +7,35 @@ const stil=StyleSheet.create(
  {
    enunt:{
      marginHorizontal:16,
-     marginVertical:8,
-     borderRadius: 10,
+     marginVertical:15,
      alignItems:'center',
      fontSize:15,
+     fontWeight:'bold',
    },
    varianta:{
      marginHorizontal:16,
      marginVertical:8,
      flexDirection:'row',
-     fontSize:14,
+     fontSize:12,
      borderRadius: 10,
      padding:10,
      alignItems:'center',
    },
    container: {
      flex: 1,
-     marginTop: StatusBar.currentHeight || 0,
+     backgroundColor:'#FFFFFF'
    },
    patratel:{
-     height:10,
-     width:10,
-     borderRadius: 3,
+     height:12,
+     width:12,
+     borderRadius:4,
+     borderColor:'black',
+     borderWidth:2,
+   },
+   raspuns:{
+     fontSize:13,
+     fontWeight:'bold',
+     marginHorizontal:4,
    }
  }
 )
@@ -39,38 +46,43 @@ export default function GrilaRaresScreen({ navigation, route }) {
  const [isV3Checked, setV3] = useState(false);
  const [isV4Checked, setV4] = useState(false);
  const [isV5Checked, setV5] = useState(false);
+ const [apasatVerifica,setApasat]=useState(false);
 
  return(
  <SafeAreaView style={stil.container}>
    <ScrollView>
      <Text style={stil.enunt}>{route.params.enunt}</Text>
-     <TouchableOpacity style={[stil.varianta, {backgroundColor: !isV1Checked ? "#e6f0fc":"#a4d5ed"}]} onPress={() => setV1(!isV1Checked)}>
+     <TouchableOpacity disabled={apasatVerifica} style={[stil.varianta, {backgroundColor: !isV1Checked ? "#e6f0fc":"#a4d5ed"}]} onPress={() => setV1(!isV1Checked)}>
        <View style={[stil.patratel,{backgroundColor: !isV1Checked ? "#e6f0fc":'black'}]}></View>
-       <Text style={{marginHorizontal:10}}>{route.params.variante[0]}</Text>
-       <Text>{isV1Checked==route.params.raspunsuri[0] ? null:null}</Text>
+       <Text style={{marginHorizontal:10,flex:1}}>{route.params.variante[0]}</Text>
+       <Text style={[stil.raspuns,{color: isV1Checked==route.params.raspunsuri[0] ? 'green':'red'}]}>{!apasatVerifica ? null: isV1Checked==route.params.raspunsuri[0] ? 'Corect':'Gresit'}</Text>
      </TouchableOpacity>
-     <TouchableOpacity style={[stil.varianta, {backgroundColor: !isV2Checked ? "#e6f0fc":"#a4d5ed"}]} onPress={() => setV2(!isV2Checked)}>
+     <TouchableOpacity disabled={apasatVerifica} style={[stil.varianta, {backgroundColor: !isV2Checked ? "#e6f0fc":"#a4d5ed"}]} onPress={() => setV2(!isV2Checked)}>
        <View style={[stil.patratel,{backgroundColor: !isV2Checked ? "#e6f0fc":'black'}]}></View>
-       <Text style={{marginHorizontal:10}}>{route.params.variante[1]}</Text>
+       <Text style={{marginHorizontal:10,flex:1}}>{route.params.variante[1]}</Text>
+       <Text style={[stil.raspuns,{color: isV2Checked==route.params.raspunsuri[1] ? 'green':'red'}]}>{!apasatVerifica ? null: isV2Checked==route.params.raspunsuri[1] ? 'Corect':'Gresit'}</Text>
      </TouchableOpacity>
-     <TouchableOpacity style={[stil.varianta, {backgroundColor: !isV3Checked ? "#e6f0fc":"#a4d5ed"}]} onPress={() => setV3(!isV3Checked)}>
+     <TouchableOpacity disabled={apasatVerifica} style={[stil.varianta, {backgroundColor: !isV3Checked ? "#e6f0fc":"#a4d5ed"}]} onPress={() => setV3(!isV3Checked)}>
        <View style={[stil.patratel,{backgroundColor: !isV3Checked ? "#e6f0fc":'black'}]}></View>
-       <Text style={{marginHorizontal:10}}>{route.params.variante[2]}</Text>
+       <Text style={{marginHorizontal:10,flex:1}}>{route.params.variante[2]}</Text>
+       <Text style={[stil.raspuns,{color: isV3Checked==route.params.raspunsuri[2] ? 'green':'red'}]}>{!apasatVerifica ? null: isV3Checked==route.params.raspunsuri[2] ? 'Corect':'Gresit'}</Text>
      </TouchableOpacity>
-     <TouchableOpacity style={[stil.varianta, {backgroundColor: !isV4Checked ? "#e6f0fc":"#a4d5ed"}]} onPress={() => setV4(!isV4Checked)}>
+     <TouchableOpacity disabled={apasatVerifica} style={[stil.varianta, {backgroundColor: !isV4Checked ? "#e6f0fc":"#a4d5ed"}]} onPress={() => setV4(!isV4Checked)}>
        <View style={[stil.patratel,{backgroundColor: !isV4Checked ? "#e6f0fc":'black'}]}></View>
-       <Text style={{marginHorizontal:10}}>{route.params.variante[3]}</Text>
+       <Text style={{marginHorizontal:10,flex:1}}>{route.params.variante[3]}</Text>
+       <Text style={[stil.raspuns,{color: isV4Checked==route.params.raspunsuri[3] ? 'green':'red'}]}>{!apasatVerifica ? null: isV4Checked==route.params.raspunsuri[3] ? 'Corect':'Gresit'}</Text>
      </TouchableOpacity>
-     <TouchableOpacity style={[stil.varianta, {backgroundColor: !isV5Checked ? "#e6f0fc":"#a4d5ed"}]} onPress={() => setV5(!isV5Checked)}>
+     <TouchableOpacity disabled={apasatVerifica} style={[stil.varianta, {backgroundColor: !isV5Checked ? "#e6f0fc":"#a4d5ed"}]} onPress={() => setV5(!isV5Checked)}>
        <View style={[stil.patratel,{backgroundColor: !isV5Checked ? "#e6f0fc":'black'}]}></View>
-       <Text style={{marginHorizontal:10}}>{route.params.variante[4]}</Text>
+       <Text style={{marginHorizontal:10,flex:1}}>{route.params.variante[4]}</Text>
+       <Text style={[stil.raspuns,{color: isV5Checked==route.params.raspunsuri[4] ? 'green':'red'}]}>{!apasatVerifica ? null: isV5Checked==route.params.raspunsuri[4] ? 'Corect':'Gresit'}</Text>
      </TouchableOpacity>
      <View style={stil.varianta}>
       <View style={{flex:1}}>
-       <Button title='Verifica' color='#8ef595'/>
+       <Button disabled={apasatVerifica} title='Verifica' color='magenta' onPress={()=>setApasat(!apasatVerifica)}/>
       </View>
       <View style={{flex:1}}>
-       <Button title='Continua' color='#86d1f7'/>
+       <Button title='Continua' color='#86d1f7' onPress={()=>setApasat(!apasatVerifica)}/>
       </View>
      </View>
    </ScrollView>
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,73 @@
-import * as React from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, Button, View } from "react-native";
+
+const DATA = [
+  {
+    id: "1",
+    title: "Parcurge materia",
+  },
+  {
+    id: "2",
+    title: "Reguli mnemonice",
+  },
+  {
+    id: "3",
+    title: "Rezolva grile",
+  },
+  {
+    id: "4",
+    title: "Gluma zilei",
+  },
+];
+
+const Item = ({ item, onPress, style }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+    <Text style={styles.title}>{item.title}</Text>
+  </TouchableOpacity>
+);
+
 
 export default function HomeScreen({ navigation }) {
-  return (
-    <Button
-      title="Go to Jane's profile"
-      onPress={() =>
-        navigation.navigate('Rezolva Grile', { enunt: 'Necroza miocardică poate fi demonstrată prin (pag. 135):', variante: ['detectarea creșterii în sânge a markerilor de necroză miocardică', 'demonstrarea pierderii de miocard viabil folosind diverse metode imagistice', 'detectarea prezenței undelor Q patologice noi pe electrocardiogramă', 'examen morfopatologic', 'pe baza tabloului clinic'], raspunsuri: [true, false, true, false, false] })
+  const [selectedId, setSelectedId] = useState(null);
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+    const navigheaza=()=>{
+      if(item.id==3){
+        return(navigation.navigate('Rezolva Grile', { enunt: 'Necroza miocardică poate fi demonstrată prin (pag. 135):', variante: ['detectarea creșterii în sânge a markerilor de necroză miocardică', 'demonstrarea pierderii de miocard viabil folosind diverse metode imagistice', 'detectarea prezenței undelor Q patologice noi pe electrocardiogramă', 'examen morfopatologic', 'pe baza tabloului clinic'], raspunsuri: [true, false, true, false, false] }));
       }
-    />
+    };
+    return (
+      <Item
+        item={item}
+        onPress={() => navigheaza()}
+        style={{ backgroundColor }}
+        />
+      );
+    };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
   );
 };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    item: {
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+      borderRadius:10,
+    },
+    title: {
+      fontSize: 16,
+    },
+  });
