435966dba8618894dd343d7d7f2c570d34253ac4
mateimanea <56696113+mateimanea@users.noreply.github.com>
2020-12-28 18:34:44 +0200

    bancul zilei

---
+++ screens/BanculZilei.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import {Text} from 'react-native';
+
+export default function BanculZilei ({ navigation, route }) {
+  return <Text>Un om mergea linistit prin desert cand deodata ia o palma. Se uita in dreapta, in stanga, in spate, nimeni. Se uita in sus: senin.
+Mai merge si iarasi ia o palma. Se uita in dreapta, in stanga, in spate, nimeni. Se uita in sus: senin.
+Merge mai departe: la fel, iarasi ia o palma. Se uita in dreapta, in stanga, in spate, nimeni. Se uita in sus: senin.
+Si asa a luat trei palme din senin.</Text>;
+};
